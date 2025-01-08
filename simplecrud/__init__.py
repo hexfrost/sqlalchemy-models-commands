1866e81570fce4c6fa63a3d53c09cd7822bea7d1
Ilia Kaziamov <kaziamov@outlook.com>
2025-01-09 00:41:34 +0400

    update tests

--- simplecrud/__init__.py
+++ simplecrud/__init__.py
@@ -1,1 +1,5 @@
 from . import crud
+
+from .asyncpg_commands.cruds import insert_to_table
+
+
---
+++ tests/test_postgres_scheme.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test_table (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+);
